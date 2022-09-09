0ba35fa9c07f3c91cc983b42648fe027ca42e6f4
unknown <kumarjitu199@gmail.com>
2022-09-10 03:52:07 +0530

    coloumn and cards added

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,64 @@
+import 'package:expense_planner/transaction.dart';
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final List<Transaction> transaction = [
+    Transaction(
+      id: 't1',
+      title: 'new Shoes',
+      amount: 650,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'WEEKLY SHOPPING',
+      amount: 250,
+      date: DateTime.now(),
+    ),
+  ];
   @override
-  Widget build( BuildContext context) {
+  Widget build(BuildContext context) {
     return MyHomePage();
   }
 }
-class MyHomePage extends StatelessWidget {
 
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-          home: Scaffold(
-          appBar: AppBar(
-            title: Text('Flutter App'),
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 18,
+          title: Center(
+            child: Text(
+              'EXPENSE PLANNER',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
           ),
-          body: Column(
-            children: [
-              Card(
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Card(
+              elevation: 5,
+              color: Colors.grey,
+              child: Container(
+                width: double.infinity,
                 child: Text('CHART!'),
               ),
-              Card(
-                child: Text('LIST OF TX'),
-              ),
-            ],
-          )),
-        );;
+            ),
+            //Column(children: Transaction.map((){}).toList,),
+          ],
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+    ;
   }
 }
