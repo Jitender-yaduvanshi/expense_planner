eae843b1a4a7ee774ca25eae23723af8cec8375d
unknown <kumarjitu199@gmail.com>
2022-09-19 00:16:30 +0530

    changes color and calender added

--- lib/main.dart
+++ lib/main.dart
@@ -61,11 +61,11 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  void _addNewTransaction(String txTitle, double txAmount) {
+  void _addNewTransaction(String txTitle, double txAmount,DateTime chosenDate) {
     final newTx = Transaction(
       title: txTitle,
       amount: txAmount,
-      date: DateTime.now(),
+      date: chosenDate,
       id: DateTime.november.toString(),
     );
     setState(
@@ -81,7 +81,7 @@ class _MyHomePageState extends State<MyHomePage> {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           primarySwatch: Colors.deepPurple, accentColor: Colors.amberAccent),
-      home: Scaffold(
+      home: Scaffold( backgroundColor: Colors.grey,
         appBar: AppBar(
           elevation: 18,
           title: Center(
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../models/transaction.dart';
@@ -12,42 +13,60 @@ class TransactionList extends StatelessWidget {
       child: ListView(
         children: transactions.map((tx) {
           return Card(
+            elevation: 10,
             child: Row(
               children: [
                 Container(
                   padding: EdgeInsets.all(10),
                   decoration: BoxDecoration(
-                    border: Border.all( color: Theme.of(context).primaryColorDark, width: 3),
+                    border: Border.all(
+                      color: Theme.of(context).primaryColorDark,
+                      width: 4,
+                    ),
+                    borderRadius: BorderRadius.circular(50),
                   ),
                   margin: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
                   child: Text(
-                    ' ₹ ${tx.amount.toStringAsFixed(2)}',
+                    '  ${tx.amount.toStringAsFixed(2)}',
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 20,
                         color: Theme.of(context).primaryColorDark),
                   ),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      tx.title,
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.black87,
-                        fontWeight: FontWeight.bold,
+                Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tx.title,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black87,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    Text(
-                      DateFormat('yyyy-MM-dd').format(tx.date),
-                      style: TextStyle(
-                        fontSize: 10,
-                        color: Theme.of(context).primaryColorDark,
-                        fontWeight: FontWeight.bold,
+                      Text(
+                        DateFormat('yyyy-MM-dd').format(tx.date),
+                        style: TextStyle(
+                          fontSize: 10,
+                          color: Theme.of(context).primaryColorDark,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                  ],
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          IconButton(
+                            alignment: AlignmentDirectional.centerEnd,
+                            onPressed: () {},
+                            icon: Icon(Icons.delete),
+                            color: Colors.red,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 )
               ],
             ),
